5895134e49991c92a304c8cbb70132c943a601d2
Ivan Calderon <ivancho_1096@hotmail.com>
2024-02-12 19:48:07 -0500

    configurando y agregando AdminLTE

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,12 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', 'Dashboard::index');
+
+
+//Tareas
+$routes->get('AddTask','CTask::index');
+$routes->post('SaveTask','CTask::save');
+$routes->get('DeleteTask/(:num)', 'CTask::delete/$1');
+$routes->get('EditTask/(:num)', 'CTask::edit/$1');
+$routes->post('UpdateTask','CTask::update');
